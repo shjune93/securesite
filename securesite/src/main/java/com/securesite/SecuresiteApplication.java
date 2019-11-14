b35ca907017614d6dfc420dcf9080658850f7e4d
shjune93 <shjune93@gmail.com>
2019-11-14 14:41:36 +0900

    test

---
+++ securesite/src/main/java/com/securesite/SecuresiteApplication.java
@@ -0,0 +1,13 @@
+package com.securesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuresiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuresiteApplication.class, args);
+	}
+
+}
