f2ae387a965883bc3a8708a96b9541b32d8ae4b1
shjune93 <shjune93@gmail.com>
2020-06-07 15:45:07 +0900

    h2db war배포

--- securesite/src/main/java/net/securesite/SecuresiteApplication.java
+++ securesite/src/main/java/net/securesite/SecuresiteApplication.java
@@ -1,13 +1,21 @@
 package net.securesite;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.boot.builder.SpringApplicationBuilder; 
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SecuresiteApplication {
+public class SecuresiteApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecuresiteApplication.class, args);
 	}
+
+	
+	@Override 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SecuresiteApplication.class); 
+		}
 
 }
